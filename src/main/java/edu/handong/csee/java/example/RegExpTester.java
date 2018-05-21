6ad7ec77825348876d34254d702aff5169cf332e
JC <JC jc@lifove.net>
2018-05-21 11:20:14 +0900

    Update example

--- src/main/java/edu/handong/csee/java/example/RegExpTester.java
+++ src/main/java/edu/handong/csee/java/example/RegExpTester.java
@@ -1,5 +1,8 @@
 package edu.handong.csee.java.example;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RegExpTester {
 
 	public static void main(String[] args) {
@@ -8,7 +11,44 @@ public class RegExpTester {
 	}
 
 	private void run() {
+		String myStringToParse = "-------- 2018년 5월 21일 --------";
 		
+		String pattern = "([0-9]+).\\s([0-9]+).\\s([0-9]+)";
+		Pattern r = Pattern.compile(pattern);
+		Matcher m = r.matcher(myStringToParse);
+
+		int year = 0;
+		int month = 0;
+		int day = 0;
+		if (m.find()) {
+		
+			year = Integer.parseInt(m.group(1));
+			month = Integer.parseInt(m.group(2));
+			day = Integer.parseInt(m.group(3));
+		}
+			
+		System.out.println(year + "-" + String.format("%02d", month) + "-" + day);
+		
+		
+		String myStringToParse2 = "[오전 9:01]";
+		
+		pattern = "(.{2})\\s([0-9]+):([0-9]+)";
+		r = Pattern.compile(pattern);
+		m = r.matcher(myStringToParse2);
+		
+		String ampm = "";
+		String hour = "";
+		String min = "";
+		if (m.find()) {
+			ampm = m.group(1);
+			hour = m.group(2);
+			min = m.group(3);
+			
+		}
+		
+		System.out.println(ampm + " " + hour + " " + min);
+		
+		String myStringToParse3 = "[9:01 AM]";
 	}
 
 }
